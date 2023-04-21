57af109cafba286d729d2004cc9a09dbebfcc641
dempo-ruku <ruku61oma@gmail.com>
2023-04-21 11:41:53 +0900

    add: 画像追加

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Shop;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use InterventionImage;
 
 
 class ShopController extends Controller
@@ -15,13 +17,13 @@ class ShopController extends Controller
         $this->middleware('auth:owners');
 
         $this->middleware(function($request, $next){
-            $id = $request->route()->parameter('shop'); //shopのid取得
-            if(!is_null($id)){ // null判定
+            $id = $request->route()->parameter('shop');
+            if(!is_null($id)){
                 $shopsOwnerId = Shop::findOrFail($id)->owner->id;
-                $shopId = (int)$shopsOwnerId; // キャスト 文字列→数値に型変換
+                $shopId = (int)$shopsOwnerId;
                 $ownerId = Auth::id();
-                if($shopId !== $ownerId){ // 同じでなかったら
-                    abort(404); // 404画面表示
+                if($shopId !== $ownerId){
+                    abort(404);
                 }
             }
             return $next($request);
@@ -37,11 +39,20 @@ class ShopController extends Controller
 
     public function edit($id)
     {
-        dd(Shop::findOrFail($id));
+        $shop = Shop::findOrFail($id);
+        return view('owner.shops.edit', compact('shop'));
     }
 
     public function update(Request $request, $id)
     {
+        $imageFile = $request->image;
+        if(!is_null($imageFile) && $imageFile->isValid()) {
+            $fileName = uniqid(rand().'_');
+            $extension = $imageFile->extension(); $fileNameToStore = $fileName. '.' . $extension;
+            $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
+            Storage::put('public/shops/' . $fileNameToStore, $resizedImage );
+        }
 
+        return redirect()->route('owner.shops.index');
     }
 }
