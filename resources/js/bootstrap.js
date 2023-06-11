199769f9b9765d1ac39980c571d9322fe2d277c6
dempo-ruku <ruku61oma@gmail.com>
2023-06-11 12:11:37 +0900

    fix: モーダル表示修正

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,7 +11,7 @@ window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 
-import MicroModal from 'micromodel';
+import MicroModal from 'micromodal';
 
 MicroModal.init({
     disableScroll :true,
