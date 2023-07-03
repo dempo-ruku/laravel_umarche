35491aac69f6e532c62fda49ceef5433c501a875
dempo-ruku <ruku61oma@gmail.com>
2023-07-03 20:44:58 +0900

    add: 商品一覧factory

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -9,22 +9,22 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <div class="flex flex-wrap">
+                        @foreach($products as $product)
+                        <div class="w-1/4 p-2 md:p-4">
+                            <a href="">
+                            <div class="border rounded-md p-2 md:p-4">
+                                <x-thumbnail filename="{{ $product->imageFirst->filename ?? ''}}" type="products"/>
+                                <div class="text-gray-700">{{ $product->name }}</div>
+                            </div>
+                            </a>
+                        </div>
+                        @endforeach
+                </div>
                 </div>
             </div>
         </div>
     </div>
 </x-app-layout>
 
-<div class="flex flex-wrap">
-        @foreach($products as $product)
-        <div class="w-1/4 p-2 md:p-4">
-            <a href="">
-            <div class="border rounded-md p-2 md:p-4">
-                <x-thumbnail filename="{{ $product->imageFirst->filename ?? ''}}" type="products"/>
-                <div class="text-gray-700">{{ $product->name }}</div>
-            </div>
-            </a>
-        </div>
-        @endforeach
-</div>
+
